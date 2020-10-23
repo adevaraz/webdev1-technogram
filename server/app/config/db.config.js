68f705c054f5d05fd1c2f16d02fc51f1ec54624a
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2020-10-23 18:00:04 +0700

    create berita model

---
+++ server/app/config/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  HOST: "ec2-34-232-212-164.compute-1.amazonaws.com",
+  USER: "qsdkjzxmeagawi",
+  PASSWORD: "d101b4636161d83d2652516bda34637f6431d8e4f4dfaf6d5ef131491edc2b93",
+  DB: "dae6sui9jl639r",
+  dialect: "postgres",
+  port: 5432,
+};
