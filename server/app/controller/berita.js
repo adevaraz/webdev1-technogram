60c52846cd370f4a757c41bd837eeb8f16845163
Nadia Melyani <ndmlni591@gmail.com>
2020-10-27 13:36:03 +0700

    Update berita.js

--- server/app/controller/berita.js
+++ server/app/controller/berita.js
@@ -86,7 +86,7 @@ exports.getByCat = async(req, res, next) => {
  Mengambil semua berita
 */
 
-exports.getAllNews = async(req, res) => {
+exports.getAllNews = async(req, res, next) => {
     try {
         const artikel = await Berita.findAll()
         if(artikel.length > 0) {
@@ -95,7 +95,7 @@ exports.getAllNews = async(req, res) => {
                 data : artikel
             })
         } else {
-            res.status(200).send({
+            res.status(404).send({
                 message: 'Articles not Found'
             })
         }
