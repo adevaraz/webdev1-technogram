bba95b2babfc0b3bbbfa29dad597dcf155374be7
adevaraz <zaraveda.zv@gmail.com>
2020-11-08 17:13:13 +0700

    fix error require file

--- server/app/route/kategori.js
+++ server/app/route/kategori.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const kategoriController = require("../controller/kategori");
-const authenticate = require('../middleware/adminAuth');
+const authenticate = require('../middleware/authentication');
 const {kategoriValidationRules} = require('../middleware/validators/kategori');
 const {validate} = require('../middleware/validation');
 
 const router = express.Router();
 
-router.post('/create' , authenticate , kategoriValidationRules() , validate , kategoriController.createKategori);
+router.post('/create' , authenticate, kategoriValidationRules() , validate , kategoriController.createKategori);
 
-router.delete('/delete/:id' , authenticate , kategoriController.deleteKategoriByID);
+router.delete('/delete/:id' , authenticate, kategoriController.deleteKategoriByID);
 
 router.get('/get/:id' , kategoriController.getKategoriByID);
 
