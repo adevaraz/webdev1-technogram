2cd6805f08946703ced9afd3c05e87dfe034f265
alviraputrinad <48423400+alviraputrinad@users.noreply.github.com>
2020-11-12 21:29:26 +0700

    Update AddAdmin.vue
    versi2

--- client/technogram/src/components/admin/berita/AddAdmin.vue
+++ client/technogram/src/components/admin/berita/AddAdmin.vue
@@ -5,13 +5,36 @@
         <v-text-field label="Username" v-model= "username"></v-text-field>
         <v-text-field :type="'password'" label="Password" v-model="password"> </v-text-field>
       </v-form>
-      <v-btn elevation="5" small>Add New Admin</v-btn>
+      <v-btn elevation="5" small @click="addAdmin">Add New Admin</v-btn>
       </div>
     </v-app>
  
 </template>
 
 <script>
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      error: {
+        isError: false,
+        message: "",
+      },
+      rules : {
+        username: (value) => !!value || "Username tidak boleh kosong",
+        password: (value) => !!value || "Password tidak boleh kosong",
+      },
+      }
+    },
+    methods:{
+      async addAdmin(){
+        console.log(this.username);
+        
+      }
+    }
+  }
 
 </script>
 
