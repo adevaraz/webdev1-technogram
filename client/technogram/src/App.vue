ebd433bbb8e89800c88cd963abec585195f0f1cc
alviraputrinad <48423400+alviraputrinad@users.noreply.github.com>
2020-11-12 18:11:59 +0700

    Versi 1

--- client/technogram/src/App.vue
+++ client/technogram/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="app">
     <navbar></navbar>
+    <login-admin></login-admin>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/admin/ui/Navbar"
+import AddAdmin from './components/admin/berita/AddAdmin.vue';
 export default {
   components: {
-    "navbar": Navbar
+    "navbar": Navbar,
+    "login-admin" : AddAdmin
   }
 };
 </script>
---
+++ client/technogram/src/components/admin/berita/AddAdmin.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app>
+    <div>
+      <v-form>
+        <v-text-field label="Username" v-model= "username"></v-text-field>
+        <v-text-field :type="'password'" label="Password" v-model="password"> </v-text-field>
+      </v-form>
+      <v-btn elevation="5" small>Add New Admin</v-btn>
+      </div>
+    </v-app>
+ 
+</template>
+
+<script>
+
+</script>
+
+<style>
+  
+</style>
