3057d873dde3fbf4cfda6ea9ae07a62c9b6008dc
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-11-13 20:36:53 +0700

    Delete vue-config.js

--- client/technogram/vue-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    devServer: {
-      open: process.platform === 'darwin',
-      host: '0.0.0.0',
-      port: 8085, // CHANGE YOUR PORT HERE!
-      https: true,
-      hotOnly: false,
-    },
-  }
