9bc3cc8e9c93edf93ad3c2aa271e479de29a867a
Nadia Melyani <ndmlni591@gmail.com>
2020-11-14 18:08:38 +0700

    read berita

--- client/technogram/src/App.vue
+++ client/technogram/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="app">
-    <navbar></navbar>
-  </div>
+<div id="app">
+  <v-app>
+    <daftar-berita></daftar-berita>
+  </v-app>
+</div>
 </template>
 
 <script>
-import Navbar from "./components/admin/ui/Navbar"
+//import Navbar from "./components/admin/ui/Navbar"
+import DaftarBerita from './components/admin/berita/DaftarBerita.vue';
 export default {
   components: {
-    "navbar": Navbar
+  //  "navbar": Navbar, <navbar></navbar>
+    DaftarBerita
   }
 };
 </script>
---
+++ client/technogram/src/components/admin/berita/DaftarBerita.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-card width="1000px">
+    <v-card-title>
+      Berita
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="berita"
+      :pagination.sync="pagination"
+      select-all
+      item-key="name"
+      class="elevation-1"
+    >
+      <template v-slot:headers="props">
+        <tr>
+          <th
+            v-for="header in props.headers"
+            :key="header.text"
+          >
+            {{ header.text }}
+          </th>
+          <th>
+            Pilihan
+          </th>
+        </tr>
+      </template>
+      <template v-slot:items="props">
+        <tr :active="props.selected" @click="props.selected = !props.selected">
+          <td class="text-xs-center">{{ props.item.id_berita }}</td>
+          <td class="text-xs-center">{{ props.item.judul_berita }}</td>
+          <td class="text-xs-center">{{ props.item.waktu_publikasi }}</td>
+          <td class="text-xs-center">{{ props.item.kategori }}</td>
+          <td class="text-xs-center">
+            <v-btn color="success" dark>
+                Publish
+            </v-btn>
+            <v-btn color="warning" dark>
+                Edit
+            </v-btn>
+            <v-btn color="error" dark>
+                Delete
+            </v-btn>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      pagination: {
+      sortBy: 'id'
+    },
+    selected: [],
+    headers: [
+          {
+            text: 'id',
+            align: 'left',
+            sortable: false,
+            value: 'id_berita',
+          },
+          { text: 'Judul', value: 'judul_berita'},
+          { text: 'Waktu Publikasi', value: 'waktu_publikasi' },
+          { text: 'Kategori', value: 'kategori' }
+        ],
+        berita: [
+          {
+            id_berita: 1,
+            judul_berita: 'judul#1',
+            waktu_publikasi: null,
+            kategori: 'AI',
+          },
+          {
+            id_berita: 2,
+            judul_berita: 'judul#2',
+            waktu_publikasi: null,
+            kategori: 'Mobile Apps',
+          },
+          {
+            id_berita: 3,
+            judul_berita: 'judul#3',
+            waktu_publikasi: null,
+            kategori: 'Operating Systems',
+          },
+        ],
+      }
+    },
+  }
+</script>
