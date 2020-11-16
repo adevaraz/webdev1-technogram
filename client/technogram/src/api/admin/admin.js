c8fcdae6a7e881ae4fed0508fa31dfc75f029cbb
alviraputrinad <48423400+alviraputrinad@users.noreply.github.com>
2020-11-16 20:08:31 +0700

    Update admin.js

--- client/technogram/src/api/admin/admin.js
+++ client/technogram/src/api/admin/admin.js
@@ -4,14 +4,9 @@ import ErrorHandler from '../errorHandler'
 const addAdmin = async(username, password)=>{
     try{
     const addAdminURL = ADMIN_URL + '/new-admin'
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOiJcImFkbWluXCI7IiwiaWF0IjoxNjA1NTIwNTE4LCJleHAiOjE2MDU1MjQxMTh9.9OhWCj2IyJiGuUoEYmHJOvKb7lHW17ivAcxfohEk9es"
     const result = await axios.post(addAdminURL, {
         username,
         password
-    },{
-        headers:{
-            'Authorization' : `${token}`
-        }
     },
     {
         timeout : TIMEOUT,
