9c8cf0c828b26a7a58dd90411930765772dc82ec
alviraputrinad <48423400+alviraputrinad@users.noreply.github.com>
2020-11-16 20:04:26 +0700

    ap-cms-admin

--- client/technogram/src/api/admin/admin.js
+++ client/technogram/src/api/admin/admin.js
@@ -4,7 +4,7 @@ import ErrorHandler from '../errorHandler'
 const addAdmin = async(username, password)=>{
     try{
     const addAdminURL = ADMIN_URL + '/new-admin'
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOiJcImFkbWluXCI7IiwiaWF0IjoxNjA1MzU2ODM4LCJleHAiOjE2MDUzNjA0Mzh9.Vr55xXXLwdvMnGETmRHVH9ipmW1IrgNPAHHDIq2o_Wk"
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOiJcImFkbWluXCI7IiwiaWF0IjoxNjA1NTIwNTE4LCJleHAiOjE2MDU1MjQxMTh9.9OhWCj2IyJiGuUoEYmHJOvKb7lHW17ivAcxfohEk9es"
     const result = await axios.post(addAdminURL, {
         username,
         password
--- client/technogram/src/components/admin/berita/AddAdmin.vue
+++ client/technogram/src/components/admin/berita/AddAdmin.vue
@@ -2,10 +2,16 @@
     <v-app>
  
       <v-card max-width="374">
+      <v-progress-linear
+      v-if="isLoading"
+      color="#E52B38"
+      height="10"
+      indeterminate
+    ></v-progress-linear>
         <v-card-title><h3>Create New Admin</h3></v-card-title>
           <v-card-text>
-            <v-text-field label="Username" v-model= "username" 
-            :rules="rules"
+            <v-text-field label="Username" :rules="[rules.username]" v-model= "username" 
+            
             counter
             maxlength="15"
             hint="At least 8 characters">
@@ -13,7 +19,7 @@
            <v-text-field
             v-model="password"
             :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]"
+            :rules="[rules.password]"
             :type="show ? 'text' : 'password'"
             label="Password"
             hint="At least 8 characters">
@@ -26,16 +32,10 @@
           v-if="error.isError">
           {{errorMessage}}
           </v-alert>
-          <v-alert
-          dense
-          outlined
-          type="success"
-          v-if="error.isSuccess">
-          {{successMessage}}
-          </v-alert>
-
+          <div style="padding-bottom: 20px;  text-align: center">
           <v-btn elevation="4" small color="primary" @click="addAdmin" :disabled="!isInputValid">Create New Admin</v-btn>
           <v-btn elevation="4" small  color="error">Close</v-btn>
+          </div>
        </v-card>
     </v-app>
  
@@ -47,11 +47,11 @@ export default {
   data() {
     return {
       username: "",
+       isLoading: false,
       password: "",
       error: {
         isError: false,
-        message: "",
-        isSuccess: false
+        message: ""
       },
       rules : {
         username: (value) => !!value || "Username tidak boleh kosong",
@@ -64,9 +64,6 @@ export default {
         errorMessage() {
         return this.error.message; 
         },
-        successMessage() {
-        return "Account is created";
-        },
         isInputValid() {
         const isEmpty = (this.username === "") | (this.password === "");
         return !isEmpty;
@@ -75,17 +72,16 @@ export default {
     
     methods:{
       async addAdmin(){
-        console.log(this.username);
+        this.isLoading = true;
         const result = await Admin.addAdmin(this.username, this.password);
-        console.log(result)
+        this.isLoading = false;
         if(result instanceof Error){
           this.error.message = result.cause;
           this.error.isError = true;
    
         }else{
-          
-          this.error.isSuccess = true;
-        
+           this.error.isError = false;
+           console.log(this.message)
           throw result
         }
       }
