4859c43ea635ae9789ad506c45c9c353ce538cd6
Nadia Melyani <ndmlni591@gmail.com>
2020-11-17 22:42:02 +0700

    update read berita

--- client/technogram/src/api/admin/daftarberita.js
+++ client/technogram/src/api/admin/daftarberita.js
@@ -28,7 +28,7 @@ const publish = async(id) => {
         const result = await axios.put(publishByURL, {
             timeout : TIMEOUT,
             headers: {
-                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgsInJvbGVzIjoiXCJhZG1pblwiOyIsImlhdCI6MTYwNTQ4OTg5OCwiZXhwIjoxNjA1NDkzNDk4fQ.sm6QzbAWBOKzkU5KT_34e9bg1ukRDnDVeQ6JcTxWyIM"
+                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgsInJvbGVzIjoiXCJhZG1pblwiOyIsImlhdCI6MTYwNTYyMTM0MiwiZXhwIjoxNjA1NjI0OTQyfQ.7YzYqk4TAxr6RPHjd2pUtOXyyMpKk8ELQ_B_iFQiew4"
             }
         })
         return result.data;
@@ -44,7 +44,23 @@ const deleteBy = async(id) => {
         {
             timeout : TIMEOUT,
             headers: {
-                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgsInJvbGVzIjoiXCJhZG1pblwiOyIsImlhdCI6MTYwNTQ4OTg5OCwiZXhwIjoxNjA1NDkzNDk4fQ.sm6QzbAWBOKzkU5KT_34e9bg1ukRDnDVeQ6JcTxWyIM"
+                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgsInJvbGVzIjoiXCJhZG1pblwiOyIsImlhdCI6MTYwNTYyMTM0MiwiZXhwIjoxNjA1NjI0OTQyfQ.7YzYqk4TAxr6RPHjd2pUtOXyyMpKk8ELQ_B_iFQiew4"
+            }
+        })
+        return result.data;
+    }catch(err){
+        return ErrorHandler.errorHandler(err);
+    }
+}
+
+const deleteAll = async() => {
+    try{
+        const deleteAll = NEWS_URL + '/delete-all'
+        const result = await axios.delete(deleteAll,
+        {
+            timeout : TIMEOUT,
+            headers: {
+                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgsInJvbGVzIjoiXCJhZG1pblwiOyIsImlhdCI6MTYwNTYyMTM0MiwiZXhwIjoxNjA1NjI0OTQyfQ.7YzYqk4TAxr6RPHjd2pUtOXyyMpKk8ELQ_B_iFQiew4"
             }
         })
         return result.data;
@@ -57,5 +73,6 @@ export default {
     getAll,
     searchBy,
     publish,
-    deleteBy
+    deleteBy,
+    deleteAll
 }
--- client/technogram/src/components/admin/berita/DaftarBerita.vue
+++ client/technogram/src/components/admin/berita/DaftarBerita.vue
@@ -1,57 +1,112 @@
 <template>
-  <v-card width="1200px">
-    <v-card-title>
-      Berita
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="key"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-        v-on:keydown.enter="searchBerita"
-      ></v-text-field>
-    </v-card-title>
-    <v-simple-table height="500px" width="600px" class="grey lighten-5" dense>
-    <template v-slot:default>
-    <thead>
-        <tr>
-          <th>id</th>
-          <th>Judul</th>
-          <th>Waktu Publikasi</th>
-          <th>Kategori</th>
-          <th class="text-center">Pilihan</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in berita"
-            :key="item.id_berita">
-          <td class="text-xs-center">{{ item.id_berita }}</td>
-          <td class="text-xs-center">{{ item.judul }}</td>
-          <td class="text-xs-center">{{ item.waktu_publikasi }}</td>
-          <td class="text-xs-center">{{ item.kategori_berita }}</td>
-          <td class="text-center">
-            <v-btn v-if="item.waktu_publikasi!=null"  class="ma-1" color="success" dark
-            @click.prevent="publishBerita(item.id_berita)">
-                Unpublish
-            </v-btn>
-            <v-btn v-else class="ma-1" color="success" dark
-            v-on:click="publishBerita(item.id_berita)">
-                Publish
-            </v-btn>
-            <v-btn class="ma-1" color="warning" dark>
-                Edit
-            </v-btn>
-            <v-btn class="ma-1" color="error" dark
-            @click.prevent="deleteBerita(item.id_berita)">
-                Delete
-            </v-btn>
-          </td>
-        </tr>
-      </tbody>
-    </template>
-    </v-simple-table>
-  </v-card>
+  <v-container>
+
+    <v-row>
+      <v-col>
+      <div class="text-h4">Daftar Berita</div>
+      </v-col>
+      <v-col cols="4">
+        <v-text-field
+          v-model="key"
+          append-icon="mdi-magnify"
+          label="Cari berdasarkan judul, kategori.."
+          outlined
+          class="field-size"
+          v-on:keydown.enter="searchBerita"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-simple-table height="500px" class="grey lighten-5" dense>
+        <template v-slot:default>
+        <thead>
+            <tr>
+              <th>id</th>
+              <th>Judul</th>
+              <th>Waktu Publikasi</th>
+              <th>Kategori</th>
+              <th class="text-center">Pilihan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in berita"
+                :key="item.id_berita">
+              <td class="text-xs-center">{{ item.id_berita }}</td>
+              <td class="text-xs-center">{{ item.judul }}</td>
+              <td class="text-xs-center">{{ item.waktu_publikasi }}</td>
+              <td class="text-xs-center">{{ item.kategori_berita }}</td>
+              <td class="text-center">
+                <v-btn v-if="item.waktu_publikasi!=null"  class="ma-1" color="success" outlined
+                @click.prevent="publishBerita(item.id_berita)"
+                width="110px">
+                  Unpublish
+                </v-btn>
+                <v-btn v-else class="ma-1" color="success" dark
+                v-on:click="publishBerita(item.id_berita)"
+                width="110px">
+                      Publish
+                  </v-btn>
+                <v-btn class="ma-2" color="warning" dark
+                width="110px">
+                    Edit
+                </v-btn>
+                <v-dialog
+                  v-model="dialog"
+                  persistent
+                  max-width="290"
+                  :retain-focus="false"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn class="ma-2" color="error"
+                    width="110px"
+                    v-bind="attrs"
+                    v-on="on">
+                      Delete
+                    </v-btn>
+                  </template>
+                    <v-card>
+                      <v-card-title class="text-h6">
+                        Apakah Anda yakin untuk menghapus berita ini?
+                      </v-card-title>
+                      <v-card-text></v-card-text>
+                      <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        color="grey darken-1"
+                        text
+                        @click="dialog = false"
+                      >
+                        Batal
+                      </v-btn>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="dialog = false"
+                        v-on:click="deleteBerita(item.id_berita)"
+                      >
+                        Yakin
+                      </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
+    <v-progress-circular
+      class="progressbar"
+      v-if="isLoading"
+      color="#E52B38"
+      height="10"
+      indeterminate
+    ></v-progress-circular>
+    <v-overlay :value="isLoading" absolute></v-overlay>
+  </v-container>
 </template>
 
 <script>
@@ -59,9 +114,11 @@ import daftarBerita from "../../../api/admin/daftarberita";
   export default {
     data () {
       return {
-      berita: [],
-      key: "",
-      id: ""
+        berita: [],
+        key: "",
+        id: "",
+        dialog: false,
+        isLoading: false
       }
     },
 
@@ -95,6 +152,7 @@ import daftarBerita from "../../../api/admin/daftarberita";
         daftarBerita.deleteBy(id)
         .then(response => {
           console.log(id);
+          console.log(this.id);
           this.retrieveBerita();
           console.log(response.data);
           })
@@ -103,11 +161,26 @@ import daftarBerita from "../../../api/admin/daftarberita";
           })
       },
 
+      test(id) {
+        console.log(id)
+      },
+
       publishBerita(id) {
         daftarBerita.publish(id)
         .then(response => {
           console.log(id);
           this.berita = response.data;
+          this.retrieveBerita();
+          console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          })
+      },
+
+      deleteAll() {
+        daftarBerita.deleteAll()
+        .then(response => {
           this.retrieveBerita();
           console.log(response.data);
           })
@@ -123,3 +196,11 @@ import daftarBerita from "../../../api/admin/daftarberita";
     }
   };
 </script>
+
+<style scoped>
+.progressbar {
+  position: relative;
+  bottom: 50%;
+  left: 50%;
+}
+</style>
