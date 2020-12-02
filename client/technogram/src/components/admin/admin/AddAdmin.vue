8872bf25aad193be37d3a5bc7bdb3f77f67d4ac0
alviraputrinad <48423400+alviraputrinad@users.noreply.github.com>
2020-12-02 09:57:37 +0700

    Tampilan add admin

--- client/technogram/src/components/admin/admin/AddAdmin.vue
+++ client/technogram/src/components/admin/admin/AddAdmin.vue
@@ -1,43 +1,45 @@
 <template>
-  <v-card max-width="374">
-    <v-progress-linear
+
+ <v-form ref="form" v-model="valid">
+ <v-progress-circular
       v-if="isLoading"
       color="#E52B38"
       height="10"
       indeterminate
-    >
-    </v-progress-linear>
-    
-    <v-card-title><h3>Create New Admin</h3></v-card-title>
-      <v-card-text>
-        <v-text-field label="Username" :rules="[rules.username]" v-model= "username" 
-            
-          counter
-          maxlength="15"
-          hint="At least 8 characters">
-        </v-text-field>
-        <v-text-field
-          v-model="password"
-          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.password]"
-          :type="show ? 'text' : 'password'"
-          label="Password"
-          hint="At least 8 characters">
-          </v-text-field>
-      </v-card-text>
-          
-      <v-alert
-        dense
-        outlined
-        type="error"
-        v-if="error.isError">
-        {{errorMessage}}
-        </v-alert>
-        <div style="padding-bottom: 20px;  text-align: center">
-        <v-btn elevation="4" small color="primary" @click="addAdmin" :disabled="!isInputValid">Create New Admin</v-btn>
-        <v-btn elevation="4" small  color="error">Close</v-btn>
-        </div>
-  </v-card>
+      >
+ </v-progress-circular>
+<v-container fluid class="fill-height">
+<h1>Create New Admin</h1>
+  <div class="content">
+
+             <form class="form justify-center">
+                <v-text-field label="Username" :rules="[rules.username]" v-model= "username" 
+                  counter
+                  maxlength="15"
+                  hint="At least 8 characters">
+                </v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.password]"
+                  :type="show ? 'text' : 'password'"
+                  label="Password"
+                  hint="At least 8 characters">
+                  </v-text-field>
+              <v-alert
+                dense
+                outlined
+                type="error"
+                v-if="error.isError">
+                {{errorMessage}}
+                </v-alert>
+                <div style="padding-bottom: 20px;  text-align: center">
+                <v-btn class="item" elevation="4" small color="primary" @click="addAdmin" :disabled="!isInputValid">Create New Admin</v-btn>  
+                </div>
+                </form>
+       </div>
+  </v-container>
+  </v-form>
 </template>
 
 <script>
@@ -90,5 +92,21 @@ export default {
 </script>
 
 <style>
+  .content {
   
+  padding-top: 15rem;
+  padding-right: 5rem;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.form{
+  width : 100%;
+}
+.item {
+  margin-top: 4rem;
+}
 </style>
---
+++ client/technogram/src/components/user/auth/LoginUser.vue
@@ -0,0 +1,194 @@
+<template>
+ <v-dialog max-width="600px" v-model="dialog">
+  <template v-slot:activator="{ on, attrs }">
+        <v-btn class="login_btn" color="#E52B38" small v-bind="attrs"
+          v-on="on">Sign in</v-btn>
+      </template>
+      <v-card :loading="isLoading && !isMobile" height="100%" :elevation="isMobile ? 0 : 2">
+         <img class="item img-btn" @click="dialog=false" src="../../../assets/icons/cross.png" />
+          <div :class="isMobile? 'content-mobile' : 'content'">
+          
+              <v-card-title>
+                <h1 :class="isMobile? 'playfair-font-mobile' : 'playfair-font'"> Sign in with email </h1> 
+              </v-card-title>
+               <form class="mt-10">
+                  <v-row class="jutify-center">
+                    <v-col cols="12">
+                      <p class="text-caption font-weight-bold text-center">Email</p>
+                        <v-text-field 
+                          v-model="email"
+                          color="#E52B38"
+                          single-line
+                          ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <p class="text-caption font-weight-bold text-center">Password</p>
+                        <v-text-field
+                          v-model="password"
+                          :rules="[rules.password]"
+                          :append-icon="isPasswordShown ? 'mdi-eye' : 'mdi-eye-off'"
+                          single-line
+                          :type="isPasswordShown ? 'text' : 'password'"
+                          color="#E52B38"
+                          class="input-group--focused"
+                          @click:append="isPasswordShown = !isPasswordShown"
+                        ></v-text-field>
+                    </v-col>
+                  </v-row>
+                    <v-row >
+                      <v-col cols="6">
+                        <p class="text-caption font-weight-bold ">Forgot your password?</p>
+                     </v-col>
+                    </v-row>
+                    <v-row align="center" justify="center">
+                      <p class="text-caption font-weight-bold ">Have no account</p>
+                        <signup></signup>
+                     </v-row>
+                    <v-col class="d-flex justify-center">
+                      <v-btn class="login_btn" color="#E52B38" small @click="signin">Sign in</v-btn>
+
+                    </v-col>
+                  </form>
+                </div>
+          </v-card>
+      <transition :name="transitionName">
+      <div class="drawer" v-if="shouldShowDrawer">
+        <nav-drawer
+          :isLoggedIn="isLoggedIn"
+        ></nav-drawer>
+      </div>
+    </transition>
+            
+</v-dialog>
+</template>
+
+<script>
+
+import Auth from "../../../api/pembaca/auth";
+import SignUpPembaca from "./SignUpUser.vue";
+import NavDrawer from "../ui/navigation/NavDrawer.vue";
+import {mapActions} from "vuex";
+
+export default {
+  data() {
+    return {
+      isPasswordShown: false,
+      isLoading: false,
+      dialog: false,
+      email: "",
+      password: "",
+      error: {
+        isError: false,
+        message: "",
+      },
+      rules: {
+        email: (value) => !!value || "Email cannot be null or empty",
+        password: (value) => !!value || "Password cannot be null or empty",
+      },
+    };
+  },
+  components :
+      {
+        'signup' : SignUpPembaca,
+        'nav-drawer' : NavDrawer
+      },
+  props: {
+    isLoggedIn: {
+      default: true,
+    },
+    toogleDrawer: Function,
+  },
+  computed: {
+    errorMessage() {
+      return this.error.message;
+    },
+    isInputValid() {
+      const isEmpty = (this.email === "") | (this.password === "");
+      return !isEmpty;
+    },
+    isMobile(){
+      return this.$vuetify.breakpoint.xs ? true : false
+    },
+    closeDialog(){
+      return this.dialog
+    }
+  },
+  methods: {
+    ...mapActions({
+      loggedIn : 'user/getNewToken',
+      setToken : 'user/setToken'
+      
+    }),
+    async signin() {
+    
+      this.error.isError = false;
+      this.error.message = "";
+      this.isLoading = true;
+      console.log(this.email);
+      const loginResult = await Auth.signin(this.email, this.password);
+      this.isLoading = false;
+      
+      
+      if (loginResult instanceof Error) {
+        this.error.message = loginResult.cause;
+        this.error.isError = true;
+      } else {
+         await this.setToken(loginResult.token, true);
+         this.$router.push({path : '/'});
+        
+      } 
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,600;0,900;1,400&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap");
+
+col-12 {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+.item{
+  height: 20px;
+  margin: 10px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+}
+.worksans-font {
+  font-family: "Work Sans", sans-serif;
+}
+.content {
+  padding: 5rem;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.content-mobile {
+  padding: 2rem;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.login_btn {
+  color: white;
+}
+.playfair-font-mobile {
+  font-family: "Playfair Display", serif;
+  font-size: 25px;
+}
+
+.playfair-font{
+  font-family: "Playfair Display", serif;
+}
+</style>
