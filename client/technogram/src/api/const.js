6da9351ec6144f37fd2193c9349ec176b5957917
nfadhil13 <naufalfadhil12344@gmail.com>
2020-12-02 05:54:20 +0000

    deployed

--- client/technogram/src/api/const.js
+++ client/technogram/src/api/const.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://technogram-api.technogram.tech"
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://technogram-api.technogram.tech"
+// const BASE_URL = "http://localhost:3000"
 
 
 const BERITA_URL = BASE_URL + "/news"
