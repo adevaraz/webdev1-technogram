f02297fe8972d02eaa5399bf8dd7725f56d05ed0
adevaraz <zaraveda.zv@gmail.com>
2020-12-20 10:04:07 +0700

    finish backend verification email with html

--- server/app/controller/mailer.js
+++ server/app/controller/mailer.js
@@ -2,10 +2,6 @@ require('dotenv').config()
 const nodemailer = require('nodemailer');
 const hbs = require("nodemailer-express-handlebars");
 
-let testAccount = async () => {
-    return await nodemailer.createTestAccount();
-}
-
 const smtpConfig = {
     host: 'smtp.gmail.com',
     port: 587,
@@ -18,6 +14,31 @@ const smtpConfig = {
 
 const transporter = nodemailer.createTransport(smtpConfig);
 
-module.exports = {
-    transporter
-};
+transporter.verify((err, success) => {
+    if(err) return new Error(err);
+    console.log("Your config is correct");
+});
+
+exports.verifyMail = async (email, username, token) => transporter.sendMail({
+    from: '"Technogram" <id.technogram@gmail.com>',
+    to: email,
+    subject: "Email Verifikasi Akun Technogram",
+    template: "verify",
+    context: {
+        user: username,
+        verificationToken: token
+    }
+});
+
+const handlebarOptions = {
+    viewEngine: {
+      extName: '.handlebars',
+      partialsDir: 'app/controller/mail-views/partials',
+      layoutsDir: 'app/controller/mail-views/layouts',
+      defaultLayout: ''
+    },
+    viewPath: 'app/controller/mail-views/templates',
+    extName: '.handlebars'
+  }
+  
+  transporter.use('compile', hbs(handlebarOptions));
