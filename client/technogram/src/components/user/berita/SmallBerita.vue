5036e83c621af3848f80a2938586a3bd25ccb9ba
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-01-15 19:32:32 +0700

    add click notif news functionality

--- client/technogram/src/components/user/berita/SmallBerita.vue
+++ client/technogram/src/components/user/berita/SmallBerita.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container @mouseover="onHover" @mouseleave="onLeave">
-    <v-row>
+    <v-row @click="onBeritaSelected(berita.id_berita)">
       <v-col class="pa-0" cols="4">
         <div class="img-container">
           <lazy-image
@@ -14,7 +14,10 @@
       <v-col class="pa-0" cols="7" offset="1" align-self="start">
         <v-row class="justify-start">
           <v-col cols="11" class="pa-0 ma-0">
-            <h2 class="playfair-font news-title clickable-text" :class="onHoverClass">
+            <h2
+              class="playfair-font news-title clickable-text"
+              :class="onHoverClass"
+            >
               {{ berita.judul || "" }}
             </h2>
           </v-col>
@@ -112,10 +115,10 @@ export default {
       default: "",
     },
   },
-  data(){
+  data() {
     return {
-      isOnHover: false
-    }
+      isOnHover: false,
+    };
   },
   methods: {
     onHover() {
@@ -123,6 +126,16 @@ export default {
     },
     onLeave() {
       this.isOnHover = false;
+    },
+    onBeritaSelected(id) {
+      this.$router
+        .push({
+          name: "read-berita",
+          params: { id: `${id}` },
+        })
+        .catch((err) => {
+          err;
+        });
     },
   },
   computed: {
@@ -134,7 +147,7 @@ export default {
         return "hover";
       }
       return "";
-    }
+    },
   },
 };
 </script>
