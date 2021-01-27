eda7c972dc242604a1a336b4db0fe39aa234a428
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2021-01-27 12:22:32 +0700

    feat  : Test PWA

--- client/technogram/vue.config.js
+++ client/technogram/vue.config.js
@@ -1,17 +1,17 @@
-require('esm')(module);
-// const { list } = require('./src/plugins/router');
-const { routes } = require('./src/routes'); 
+// require('esm')(module);
+// // const { list } = require('./src/plugins/router');
+// const { routes } = require('./src/routes'); 
 
-module.exports = {
-  pluginOptions: {
-      sitemap: {
-          // urls: list,
-          baseURL: 'https://technogram.tech',
-          routes
-      }
-  },
-  chainWebpack: config => {
-    // remove the prefetch plugin
-    config.plugins.delete('prefetch')
-  },
-}
+// module.exports = {
+//   pluginOptions: {
+//       sitemap: {
+//           // urls: list,
+//           baseURL: 'https://technogram.tech',
+//           routes
+//       }
+//   },
+//   chainWebpack: config => {
+//     // remove the prefetch plugin
+//     config.plugins.delete('prefetch')
+//   },
+// }
