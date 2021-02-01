0ce73f33ce60a8a37f1dd9fc128a302701178a15
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2021-02-01 20:35:54 +0700

    fix : Manifest.json move to vue-config

--- client/technogram/vue.config.js
+++ client/technogram/vue.config.js
@@ -1,16 +1,42 @@
 require = require('esm')(module);
-const { routes } = require('./src/routes'); 
+const { routes } = require('./src/routes');
 
 module.exports = {
-  pluginOptions: {
-      sitemap: {
-          // urls: list,
-          baseURL: 'https://technogram.tech',
-          routes
-      }
-  },
-  chainWebpack: config => {
-    // remove the prefetch plugin
-    config.plugins.delete('prefetch')
-  },
+    pluginOptions: {
+        sitemap: {
+            // urls: list,
+            baseURL: 'https://technogram.tech',
+            routes
+        }
+    },
+    chainWebpack: config => {
+        // remove the prefetch plugin
+        config.plugins.delete('prefetch')
+    },
+    pwa : {
+        "theme_color": "#E52B38",
+        "background_color": "#ffffff",
+        "icons": [
+            {
+                "src": "/public/img/icon/apple-touch-icon-60x60.png",
+                "sizes": "60x60",
+                "type": "image/png"
+            },
+            {
+                "src": "/public/img/icon/apple-touch-icon-76x76.png",
+                "sizes": "76x76",
+                "type": "image/png"
+            },
+            {
+                "src": "/public/img/icon/apple-touch-icon-152x152.png",
+                "sizes": "152x152",
+                "type": "image/png"
+            },
+            {
+                "src": "/public/img/icon/apple-touch-icon-180x180.png",
+                "sizes": "180x180",
+                "type": "image/png"
+            }
+        ]
+    }
 }
