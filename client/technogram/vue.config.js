484b6b333afd11cb2e1ee31aad89ae9f45c72674
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2021-02-02 08:02:03 +0700

    fix : background color

--- client/technogram/vue.config.js
+++ client/technogram/vue.config.js
@@ -14,29 +14,12 @@ module.exports = {
         config.plugins.delete('prefetch')
     },
     pwa : {
-        "theme_color": "#E52B38",
-        "background_color": "#ffffff",
-        "icons": [
-            {
-                "src": "/public/img/icon/apple-touch-icon-60x60.png",
-                "sizes": "60x60",
-                "type": "image/png"
-            },
-            {
-                "src": "/public/img/icon/apple-touch-icon-76x76.png",
-                "sizes": "76x76",
-                "type": "image/png"
-            },
-            {
-                "src": "/public/img/icon/apple-touch-icon-152x152.png",
-                "sizes": "152x152",
-                "type": "image/png"
-            },
-            {
-                "src": "/public/img/icon/apple-touch-icon-180x180.png",
-                "sizes": "180x180",
-                "type": "image/png"
-            }
-        ]
+        name: 'Technogram',
+        themeColor: '#e52b38',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: '#E52B38',
+        manifestOptions: {
+            background_color: "#ffffff"
+        }
     }
 }
